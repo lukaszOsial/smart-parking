165fbe16fea2cfba89b7ea75c27a0bb42b6e0475
Łukasz Osial <lukasz.osial@gmail.com>
2025-04-14 11:28:34 +0200

    add model files for ParkingSpot, Reservation, User and setup database connection

---
+++ src/main/java/com/lukaszosial/smartparking/model/ParkingSpot.java
@@ -0,0 +1,24 @@
+package com.lukaszosial.smartparking.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "parking_spot")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ParkingSpot {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private Integer number;
+	private Boolean available;
+}
---
+++ src/main/java/com/lukaszosial/smartparking/model/Reservation.java
@@ -0,0 +1,31 @@
+package com.lukaszosial.smartparking.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "reservation")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Reservation {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@ManyToOne
+	private ParkingSpot parkingSpot;
+	@ManyToOne
+	private User user;
+	private LocalDateTime startTime;
+	private LocalDateTime endTime;
+}
