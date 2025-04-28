a35989c6dcec5b3336478fa0ec311a7125d4949e
Łukasz Osial <lukasz.osial@gmail.com>
2025-04-28 11:45:24 +0200

    update Reservation model

--- src/main/java/com/lukaszosial/smartparking/model/Reservation.java
+++ src/main/java/com/lukaszosial/smartparking/model/Reservation.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -23,8 +24,10 @@ public class Reservation {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	@ManyToOne
+	@JoinColumn(name = "parking_spot_id", nullable = false)
 	private ParkingSpot parkingSpot;
 	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
 	private LocalDateTime startTime;
 	private LocalDateTime endTime;
